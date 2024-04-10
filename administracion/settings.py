d6b59f8d21edd3e42320c05242f88c5bf99b9479
fernandoJoseC <fernandocalderon05@gmail.com>
2024-04-10 17:51:13 -0500

    change  static root path

--- administracion/settings.py
+++ administracion/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATICFILES_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
 
 STATIC_URL = 'static/'
