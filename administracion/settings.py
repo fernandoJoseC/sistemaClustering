fd3850595df7ce1e032f3c34ad049254245f8b1e
fernandoJoseC <fernandocalderon05@gmail.com>
2024-04-10 17:38:25 -0500

    change css in static files dirs

--- administracion/settings.py
+++ administracion/settings.py
@@ -122,6 +122,9 @@ STATIC_ROOT = os.path.join(BASE_DIR)
 STATICFILES_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
 
 STATIC_URL = 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
